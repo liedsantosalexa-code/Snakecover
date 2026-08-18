1f627901c497e9728734626624773795e7479597
Alexa lied <lieda@soluciones-globales.netgit config --global user.name :Alexa Lied>
2026-08-18 13:39:52 -0400

    Feat:Creacion de comida

---
+++ Comida.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SnakeGame
+
+{
+   public class Comida
+ { 
+  public Punto Posicion { get; Private set; }
+  private Random random = new Random();
+
+ Public void GenerarNueva(int anchoMapa, int altoMapa)
+  {
+     int x = random.next(1, anchoMapa -1);
+     int y = random.next(1, altoMapa -1);
+
+    Posicion = new Punto (x, y);
+    Posicion.Dibujar(🍓);
+  }
+
+ }
+
+}
